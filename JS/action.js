14b59ba6477f982eafb949d7ab5da6192c012672
lomachinsky@ukr.net <lomachinsky@ukr.net>
2021-08-31 15:18:38 +0300

    Big Change
    Adaptive add , settings delete

--- JS/action.js
+++ JS/action.js
@@ -1,4 +1,4 @@
-let Number_of_symbols_for_speckles = 5;
+
 
 function Add(num) {
   let value = document.form.text_field.value;
@@ -20,51 +20,9 @@ function Equal() {
   let text = String(document.form.text_field.value);
   if (text) {
     text = eval(text);
-    text = Check_Number_of_symbols_for_speckles(text);
     document.form.text_field.value = text;
   }
 }
 
-/*Helper function*/
-function Set_Number_of_symbols_for_speckles() {}
 
-function Check_Number_of_symbols_for_speckles(input) {
-  let value = String(input);
 
-  if (value.indexOf(".") > -1) {
-    value = value.slice(
-      0,
-      value.indexOf(".") + (+Number_of_symbols_for_speckles + 1)
-    );
-  }
-  return value;
-}
-
-/*Slider*/
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-  output.innerHTML = this.value;
-  
-};
-
-/*Settings menu*/
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("drop-down-show");
-
-  window.onclick = function (event) {
-    if (!event.target.matches(".settings-btn")) {
-      var dropdowns = document.getElementsByClassName("drop-down");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains("drop-down-show")) {
-            openDropdown.classList.remove("drop-down-show");
-          }
-        }
-    }
-  };
-
-}
